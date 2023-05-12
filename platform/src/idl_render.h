e3976beb9c496f03c7d83633287958643630f7ea
Abel Ticona Larico/Sidia <jose.larico@sidia.com>
2023-05-12 02:50:01 -0400

    feat: add a simple render of lines

--- platform/src/idl_render.h
+++ platform/src/idl_render.h
@@ -7,11 +7,15 @@ namespace idl {
 
 	struct IDL_API Render {
 		virtual ~Render(){};
-		virtual void setColor(float r, float g, float v) = 0;
+		virtual void setClearColor(float r, float g, float v, float a = 1.0f) = 0;
+		virtual void setColor(float r, float g, float v, float a = 1.0f) = 0;
 		virtual void setViewPort(int width, int height) = 0;
 		virtual void clear() = 0;
-		virtual void getInfo(const u8*& renderer, const u8*& version, const u8*& glsl) = 0;
-		virtual void drawTriangle() = 0;
+		virtual void getInfo(const u8 *&renderer, const u8 *&version, const u8 *&glsl) = 0;
+		virtual void initSimpleDraw(u32 &vao, u32 &vbo, float *points, int size, int count, bool modo) = 0;
+		virtual void updateSimpleDraw(u32 &vbo, float *points, int size) = 0;
+		virtual void drawSimple(u32 &vao, int count) = 0;
+		virtual void endSimpleDraw(u32 &vao, u32 &vbo) = 0;
 	};
 }  // namespace idl
 #endif
--- platform/src/render/idl_render_opengl.h
+++ platform/src/render/idl_render_opengl.h
@@ -7,11 +7,15 @@ namespace idl {
 
 	struct RenderOpenGL : Render {
 		RenderOpenGL();
-		void setColor(float r, float g, float v);
+		void setClearColor(float r, float g, float v, float a);
+		void setColor(float r, float g, float v, float a);
 		void setViewPort(int width, int height);
 		void clear();
-		void getInfo(const u8*& renderer, const u8*& version, const u8*& glsl);
-		void drawTriangle();
+		void getInfo(const u8 *&renderer, const u8 *&version, const u8 *&glsl);
+		void initSimpleDraw(u32 &vao, u32 &vbo, float *points, int size, int count, bool modo);
+		void updateSimpleDraw(u32 &vbo, float *points, int size);
+		void drawSimple(u32 &vao, int count);
+		void endSimpleDraw(u32 &vao, u32 &vbo);
 	};
 }  // namespace idl
 #endif
