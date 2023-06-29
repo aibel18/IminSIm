9a18055f78826f8a2630bd735658ae1db4b04d7e
Abel Ticona Larico/Sidia <jose.larico@sidia.com>
2023-06-29 11:27:23 -0400

    feat: add move example

--- launcher/src/main.cpp
+++ launcher/src/main.cpp
@@ -15,37 +15,55 @@ public:
 
 	u32 vao;
 	u32 vbo;
-	int size;
-	int numPoint = 3;
+	int size_bytes;
 	int count;
 
-	void init() {
-		float data[] = {
-		    -0.5f, 0.0f,  0.0f,
-		    -1.0f, -1.0f, 0.0f,
-		    1.0f, -1.0f, 0.0f,
-		    0.0f, 1.0f, 0.0f,
-		    -1.0f, 0.0f, 0.0f,
-		    0.0f, 0.0f, 0.0f
-		};
+	float data[18] = {
+			-0.5f, 0.0f,  0.0f,
+			-1.0f, -1.0f, 0.0f,
+			1.0f, -1.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,
+			-1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.0f
+	};
 
-		size = sizeof(data);
-		count = size / (numPoint * sizeof(float));
+	float velocity[6] = {
+			0.01f, 0.01f, 0.01f,
+			0.01f, 0.01f, 0.01f
+	};
+
+	void init() {		
+
+		size_bytes = sizeof(data);
+		count = size_bytes / (3 * sizeof(float));
 		render->setClearColor(0.392f, 0.584f, 0.929f);
-		render->initSimpleDraw(vao, vbo, data, size, numPoint, false);
+		render->initData(vao, vbo, data, size_bytes, true);
 	}
 
 	void update() {
+		for (int i = 0; i < 6; i++) {
+			if (data[i * 3] > 1.5f) {
+				data[i * 3] = 1.5f;
+				velocity[i] *= -1;
+			}
+			if (data[i * 3] < -1.5) {
+				data[i * 3] = -1.5;
+				velocity[i] *= -1;
+			}
+
+			data[i * 3] += velocity[i];
+		}
+		render->updateData(vbo, data, size_bytes);
 	}
 
 	void draw() {
 		render->clear();
-		render->setColor(1.0f, 0.f, 0.f);
-		render->drawSimple(vao, count);
+		render->setColor(0.0f, 1.f, 0.f);
+		render->drawData(vao, count);
 	}
 
 	void end() {
-		render->endSimpleDraw(vao, vbo);
+		render->endData(vao, vbo);
 	}
 
 	void onResize(int width, int height) {
