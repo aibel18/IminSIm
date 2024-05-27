e389003f083797108c33ab4202b12e1fe5f22e8d
abel.mint <jaticona@gmail.com>
2024-05-27 01:25:36 -0400

    refactor: Test all case to render a LineRenderer

--- launcher/src/main.cpp
+++ launcher/src/main.cpp
@@ -1,5 +1,6 @@
 #include <EntryBase.h>
 #include "render/LineRenderer.h"
+#include "math/math_defines.h"
 
 class MyGame : public BaseGame {
 
@@ -14,45 +15,79 @@ public:
 		contextConf.minor = 1;
 	}
 
-	std::vector<float> points = {
-	    -0.0f, 0.0f, 0.0f,
-			-0.5f, 0.0f, 0.0f,
-			-0.5f, 1.0f, 0.0f,
-			0.5f, 1.0f, 0.0f,
-			0.5f, -1.0f, 0.0f,
-			0.0f, -1.0f, 0.0f,
-			0.0f, 0.0f, 0.0f,
+	std::vector<vec3> points = {
+	    {-0.0f, 0.0f, 0.0f},
+		{-0.5f, 0.0f, 0.0f},
+		{-0.5f, 1.0f, 0.0f},
+		{0.5f, 1.0f, 0.0f},
+		{0.5f, -1.0f, 0.0f},
+		{0.0f, -1.0f, 0.0f},
+		{0.0f, 0.0f, 0.0f}
 	};
-	LineRenderer *line;
-	float velocity = 0.0005;
+
+	float velocity = 0.0009;
 	float limit = 1.0f;
+	LineRenderer line = LineRenderer(points);;
+	LineRenderer *line2 = new LineRenderer(points);
+	LineRenderer *line3;
+	LineRenderer *line4;
+	LineRenderer *line5;
 
 	void init() {
-		line = new LineRenderer(points);
+        line5 = new LineRenderer(points);
+        LineRenderer line6 = LineRenderer(points);
 	}
 
+    int i = 0;
+    int step = 300;
 	void update() {
-		for (int i = 0; i < points.size() / 3; i++) {
+        if(i++ == step ) {
+            delete line2;
+            delete line5;
+            LineRenderer line6 = LineRenderer(points);
+        }
+        if(i == step *2 ) {
+            line3 = new LineRenderer(points);
+        }
+        if(i == step *3 ) {
+            delete line3;
+        }
+        if(i == step *4 ) {
+            LineRenderer a(points);
+            LineRenderer b(points);
+            line4 = new LineRenderer(points);
+        }
+        if(i == step *5 ) {
+            new LineRenderer(points);
+        }
 
-			if (line->getPoint(0) > limit) {
-				line->getPoint(0) = limit;
+        if(i > step *5)
+            return;
+
+		for (int i = 0; i < points.size(); i++) {
+
+			if (line.getPoint(0).x > limit) {
+				line.getPoint(0).x = limit;
 				velocity *= -1;
 			}
-			if (line->getPoint(0) < -limit) {
-				line->getPoint(0) = -limit;
+			if (line.getPoint(0).x < -limit) {
+				line.getPoint(0).x = -limit;
 				velocity *= -1;
 			}
 
-			line->getPoint(i * 3) += velocity;
+			line.getPoint(i).x += velocity;
 		}
-		line->update();
+		line.update();
 	}
 
 	void end() {
+        delete line4;
 	}
 
 	void onResize(int width, int height) {
 	}
 };
 
-MyGame myGame;
+BaseGame* createGame() {
+    return new MyGame;
+}
